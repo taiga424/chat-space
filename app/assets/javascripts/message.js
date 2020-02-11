fe4ffba47e24ffe9627460ab09109a62d70b82c5
taiga424 <taiga-hand4@ezweb.ne.jp>
2020-02-11 21:13:54 +0900

    add message ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,63 @@
+$(function(){ 
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+        `<div class="chatMessage__messageBox" data-message-id=${message.id}>
+          <div class="chatMessage__messageBox__info">
+            <div class="chatMessage__messageBox__info__user">
+              ${message.user_name}
+            </div>
+            <div class="chatMessage__messageBox__info__datetime">
+              ${message.created_at}
+            </div>
+          </div>
+          <p class="chatMessage__messageBox__text">
+            ${message.content}
+          </p>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="chatMessage__messageBox" data-message-id=${message.id}>
+          <div class="chatMessage__messageBox__info">
+            <div class="chatMessage__messageBox__info__user">
+              ${message.user_name}
+            </div>
+            <div class="chatMessage__messageBox__info__datetime">
+              ${message.created_at}
+            </div>
+          </div>
+          <p class="chatMessage__messageBox__text">
+            ${message.content}
+          </p>
+        </div>`
+      return html;
+    };
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chatMessage').append(html);
+      $('.chatMessage').animate({ scrollTop: $('.chatMessage')[0].scrollHeight});
+      $('form')[0].reset();
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    })
+    .always(function(){
+      $('.btn').prop('disabled', false);
+    })
+  })
+});
