cf72e23bde744550155148c732669866a6a77fa1
taiga424 <taiga-hand4@ezweb.ne.jp>
2020-02-11 21:13:28 +0900

    add message ajax

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
@@ -15,5 +16,8 @@ module ChatSpace
       g.test_framework false
     end
     config.i18n.default_locale = :ja
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      %Q(#{html_tag}).html_safe
+    end
   end
 end
